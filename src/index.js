2d321ffcfc1eb0edd169022683953e2ff98596b2
vibhuti prajapati <vibhutiprajapati784@gmail.com>
2026-01-21 11:14:30 +0530

    query and params in dynamic routes

--- src/index.js
+++ src/index.js
@@ -3,25 +3,13 @@ import express from "express";
 const app = express();
 
 // request handler
-/*
-  NOTE:
-  Express route paths written as strings are NOT regular expressions.
-  In newer versions of Express (using path-to-regexp internally),
-  characters like (), ?, +, * have special meaning and cannot be used
-  as regex groups inside a string route.
 
-  Writing "/u(s)?er" causes the router to fail while parsing the route,
-  resulting in a startup crash:
-
-  If pattern matching is required, use:
-    - a real RegExp route: app.get(/^\/u(s)?er$/, ...)
-    - or define multiple explicit routes instead.
-*/
-
-app.get("/u(s)?er",  (req,res)=>{
+app.get("/user/:id",  (req,res)=>{
+    console.log(req.params);
     res.send({name : "vibhuti"});
 });
 app.post("/user", (req,res)=>{
+    console.log(req.query);
     res.send("posted");
 } );
 app.use("/hello", (req, res)=>{
