a095e73b651255420a190b10f4d375fb4b79c339
vibhuti prajapati <vibhutiprajapati784@gmail.com>
2026-01-22 18:17:03 +0530

    multiple route handlers

--- src/index.js
+++ src/index.js
@@ -3,22 +3,95 @@ import express from "express";
 const app = express();
 
 // request handler
-
-app.get("/user/:id",  (req,res)=>{
-    console.log(req.params);
-    res.send({name : "vibhuti"});
-});
-app.post("/user", (req,res)=>{
-    console.log(req.query);
-    res.send("posted");
-} );
-app.use("/hello", (req, res)=>{
-    res.send("hello from server"); 
-});
-app.use("/" , (req,res)=>{
+app.use(
+  "/user3",
+  (req, res) => {
     res.send("hello vibhuti");
-});
-app.listen(3001, ()=>{
-    console.log("server running on port 3001");
-});
+  },
+  (req, res) => {
+    res.send("second response");
+  },
+);
 
+app.use(
+  "/user",
+  (req, res, next) => {
+    res.send("hello vibhuti");
+    next();
+  },
+  (req, res) => {
+    console.log("2nd response");
+    res.send("second response");
+  },
+);
+
+app.use(
+  "/user1",
+  (req, res, next) => {
+    next();
+    res.send("hello vibhuti");
+  },
+  (req, res) => {
+    console.log("2nd response");
+    res.send("second response");
+  },
+);
+
+app.use(
+  "/user2",
+  (req, res, next) => {
+    next();
+  },
+  (req, res) => {},
+);
+
+app.use(
+  "/user4",
+  (req, res) => {
+    res.send("hello vibhuti");
+  },
+  (req, res) => {
+    res.send("second response");
+    next();
+  },
+  (req, res) => {
+    res.send("third response");
+  },
+  (req, res) => {
+    res.send("fourth response");
+  },
+);
+app.use(
+  "/user5",
+  (req, res) => {
+  },
+  (req, res,next) => {
+    next();
+  },
+  (req, res) => {
+    res.send("third response");
+  },
+  (req, res) => {
+    res.send("fourth response");
+  },
+);
+
+app.use(
+  "/user6",
+  (req, res,next) => {
+    next();
+  },
+  (req, res,next) => {
+    next();
+  },
+  (req, res) => {
+    res.send("third response");
+  },
+  (req, res) => {
+    res.send("fourth response");
+  },
+);
+
+app.listen(3001, () => {
+  console.log("server running on port 3001");
+});
