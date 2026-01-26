2ea588a2bef073ae21eec4e13cf587b92d87163e
vibhuti prajapati <vibhutiprajapati784@gmail.com>
2026-01-26 13:47:30 +0530

    apis for finding user

--- src/index.js
+++ src/index.js
@@ -4,22 +4,19 @@ import User from "./models/user.js";
 import dotenv from "dotenv";
 const app = express();
 dotenv.config();
+
+app.use(express.json());
+
 app.get("/userdata", (req, res) => {
   throw new Error("hfwienong");
 });
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    console.error(err);
-    res.send("something went wrong..");
-  }
-});
-app.use(express.json());
-app.post("/signUp", (req, res) => {
+
+app.post("/signUp", async (req, res) => {
   try {
     console.log(req.body);
-    const {name, email, password} = req.body;
-    const user = new User({name, email, password});
-    user.save();
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
     res.send("saved successfully!");
   } catch (err) {
     console.error(err);
@@ -27,6 +24,60 @@ app.post("/signUp", (req, res) => {
     res.send("something went wrong...");
   }
 });
+
+app.get("/getUser", async (req, res) => {
+  try {
+    const { userEmail } = req.body.email;
+    const user = await User.find(userEmail);
+    if (!user) {
+      res.statusCode(404).send("user not found!");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    console.log(err);
+    res.send("something went wrong");
+  }
+});
+
+// get all users
+app.get("/getFeed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (!users) {
+      res.statusCode(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    console.log(err);
+    res.send("something went wrong ");
+  }
+}); 
+
+//find one 
+app.get("/findOne" , async (req, res)=>{
+  try{
+    const {userEmail}= req.body.email;
+    const user = await User.findOne(userEmail);
+    if(!user){
+      res.send("user not found");
+    }else{
+      res.send(user);
+    }
+  }catch(err){
+    res.send("something went wrong");
+  }
+});
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    console.error(err);
+    res.send("something went wrong..");
+  }
+});
+
+
 await mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -3,12 +3,10 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: {
       type: String,
-      unique: true,
     },
     email: {
       type: String,
       required: true,
-      unique: true,
     },
     password: {
       type: String,
