b025e4ec7c179a83783b0239fbaf77f9ea7ff6d6
vibhuti prajapati <vibhutiprajapati784@gmail.com>
2026-01-27 17:36:29 +0530

    logout api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -46,4 +46,8 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+authRouter.post("/logout", async (req,res)=>{
+    res.cookie("token", null, {expires : new Date(Date.now())});
+    res.send();
+});
 export {authRouter};
